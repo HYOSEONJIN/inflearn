bc89e30faa6b93d894c0cd6bcf143471a15b1326
HYOSEONJIN <71997946+HYOSEONJIN@users.noreply.github.com>
2020-11-07 20:42:25 +0900

    1107

---
+++ Python_Basic/Chapter02_01.py
@@ -0,0 +1,25 @@
+#chapter02-1
+#파이썬 완전 기초
+#print 사용법
+
+#기본출#
+print('Python Start')
+print("Python Start")
+print()
+print()
+print('''Python Start''')
+print("""Python Start""")
+print()
+
+
+#seperator 옵션
+print('p','y','t','h','o','n',sep='')
+print('010','1111','7777',sep='-')
+print('python','google.com',sep='@')
+print()
+
+
+#end옵션
+print('welcome to', end='      ')
+print('IT News', end='')
+print('Web Site')
