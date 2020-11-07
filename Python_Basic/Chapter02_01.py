1cdcb6baedef55563c9e1d28127466a963d53773
HYOSEONJIN <71997946+HYOSEONJIN@users.noreply.github.com>
2020-11-07 21:33:07 +0900

    Python Print 연습

--- Python_Basic/Chapter02_01.py
+++ Python_Basic/Chapter02_01.py
@@ -24,5 +24,13 @@ print('welcome to', end='      ')
 print('IT News', end='')
 print('Web Site')
 
+
 #File 옵션
-impor
+import sys
+print('Learn Pythone', file=sys.stdout) #stdout=콘솔아웃
+print()
+
+#format 사용 (d=정수, s=문자열, f=실수, o, x)
+print('%s %s' %('one', 'two')) # 정확하게 문자열만 와야한다
+print('{} {}'.format('one','2')) #포맷함수가 내부적으로 String인지 숫자인지 구별해줌
+print('{1} {0}'.format('one','two'))
