2ee5f886c851021f25b55559f023d5ad5926d1f6
HYOSEONJIN <71997946+HYOSEONJIN@users.noreply.github.com>
2020-11-08 18:18:09 +0900

    파이썬 숫자형 자료와 연산

--- Python_Basic/Chapter03_01.py
+++ Python_Basic/Chapter03_01.py
@@ -1,7 +1,7 @@
-#chapter03_1
-#숫자형
+# chapter03_1
+# 숫자형
 
-#파이썬 지원 자료형
+# 파이썬 지원 자료형
 """
 
 int : 정수
@@ -17,40 +17,40 @@ dict : 사전
 :
 """
 
-#데이터타입
+# 데이터타입
 
 str1 = "Python"
 bool = True
 str2 = 'Anaconda'
-float = 10.0 # 10 != 10.0
-int = 7
+float_v = 10.0 # 10 != 10.0
+int_v = 7
 dict = {
     "name" : "Machine Learning",
     "Version" : 2.0
     # key : value
 }
 
-#괄호모양에 따라 타입이 달라지므로 주의
+# 괄호모양에 따라 타입이 달라지므로 주의
 tuple = (7, 8, 9)
 tuple = 7, 8, 9
 set = {3,5,7}
 list = [str1, str2]
 
-#데이터타입출력
+# 데이터타입출력
 
 print(type(str1))
 print(type(bool))
 print(type(str2))
-print(type(float))
+print(type(float_v))
 print(type(list))
-print(type(int))
+print(type(int_v))
 print(type(dict))
 print(type(tuple))
 print(type(set))
 
 
 
-#숫자형 연산자
+# 숫자형 연산자
 """
 
 +
@@ -65,25 +65,25 @@ pow(x,y) : x의 y제곱 , x**y 도 같은뜻
 """
 print()
 
-#정수선언
+# 정수선언
 i=77
 i2 = -14
 big_int = 7777777777777777777777777777777777777777777777777777777777777
-#자바와 달리 큰수도 값을 집어넣을 수 있다
+# 자바와 달리 큰수도 값을 집어넣을 수 있다
 
-#정수출력
+# 정수출력
 print(i)
 print(i2)
 print(big_int)
 print()
 
-#실수 선언
+# 실수 선언
 f = 0.9999
 f2 = 3.141592
 f3 = -3.9
 f4 = 3/9
 
-#실수출력
+# 실수출력
 print(f)
 print(f2)
 print(f3)
@@ -112,3 +112,42 @@ print(">>>>>>>>>>> * ")
 print("i1 * i2 : ", i1*i2 )
 print("f1 * f2 : ", f1*f2)
 print("big_int1 * big_int2 : ", big_int1*big_int2)
+print()
+
+
+# 형변환 실습
+a = 3.
+b = 6
+c = .7
+d = 12.7
+
+# 데이터타입출력
+print(type(a),type(b),type(c),type(d))
+
+# 형변환
+print(float(b))
+print(int(c))
+print(int(d))
+print(int(True)) # True=1, False=0
+print(float(False))
+print(complex(3))
+print(complex('3')) #문자형 > 숫자형으로 바꾼 다음에 실행됨 (똑똑)
+print(complex(False))
+print()
+
+# 수치연산 함수
+print(abs(-7))
+
+x,y = divmod(100,8)
+print(x,y) #몫과 나머지
+
+print(pow(5,3))
+print(5**3)
+print()
+
+# Math 외부 모듈
+
+import math
+
+print(math.pi)
+print(math.ceil(5.1)) # x이상의 수 중에서 가장 작은 정수 
