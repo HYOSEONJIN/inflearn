0599d961615076842add74cf61e102f7d84a0c28
HYOSEONJIN <71997946+HYOSEONJIN@users.noreply.github.com>
2021-03-12 17:16:21 +0900

    -

---
+++ react/do-it-example/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <img src="http://www.easyspub.co.kr/images/logo_footer.png"/>
+      <div>안녕하세요</div>
+      </div>
+    )
+  }
+}
+
+export default App;
