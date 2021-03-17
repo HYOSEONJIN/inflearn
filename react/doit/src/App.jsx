80f237783656933c30ece4df99e1b785946c23dc
HYOSEONJIN <71997946+HYOSEONJIN@users.noreply.github.com>
2021-03-17 14:55:28 +0900

    component

---
+++ react/doit/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import StateExample from "./03/StateExample";
+
+class App extends React.Component {
+  // eslint-disable-line no-unused-vars
+  render() {
+    return (
+      <div>
+        <StateExample />
+      </div>
+    );
+  }
+}
+
+export default App;
